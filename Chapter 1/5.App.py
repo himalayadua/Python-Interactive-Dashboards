3e3877d7266da489bf1069887dbe6e5da39e10a7
himalayadua <himalaya.dua@gmail.com>
2024-06-16 10:35:42 -0700

    jun16 c4

---
+++ Chapter 1/5.App.py
@@ -0,0 +1,14 @@
+from dash import Dash, html, dcc, Input, Output
+
+app = Dash()
+
+app.layout = html.Div([
+    dcc.Input(id='input_text', value='change this text', type='text'),
+    html.Div(children='', id='output_text')
+])
+
+#Decorator
+@app.callback(
+    Output(component_id='output_text', component_property='children'),
+    Input(component_id='input_text', component_property='value')
+)
