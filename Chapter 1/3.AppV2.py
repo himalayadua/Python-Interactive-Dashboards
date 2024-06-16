20f87d51d7e2c31be4c800c20825a51fe631dcfe
himalayadua <himalaya.dua@gmail.com>
2024-06-16 05:56:43 -0700

    jun16 c3

---
+++ Chapter 1/3.AppV2.py
@@ -0,0 +1,38 @@
+from dash import Dash, html
+import pandas as pd
+import plotly.express as px
+
+happiness = pd.read_csv('world_happiness.csv')
+
+#create line graph variable
+line_fig = px.line(happiness[happiness['country']=='United States'],
+                                 x='year', y='happiness_score',
+                                 title='Happiness Scpre')
+
+app = Dash()
+
+app.layout = html.Div([
+
+    html.H1('My Dashboard'),
+    html.P(['This dashboard shows the happiness score.',
+            html.Br(),
+            html.A('World Happiness Report Data Source',
+                   href='http://worldhappiness.report',
+                   target='_blank')]),
+        
+        # add radio items
+        # dcc.RadioItems(options, value)
+        # options = list of unique values
+        # happiness['region'].unique()
+        dcc.RadioItems(options=happiness['region'].unique(), value='North America'),
+        dcc.Checklist(options=happiness['region'].unique(), value='North America'),
+        dcc.Dropdown(options=happiness['region'].unique(), value='North America'),
+        
+        #use the new variable
+        dcc.Graph(figure=line_fig)
+        
+           
+ ])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
